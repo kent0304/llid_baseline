f61d8951d39056efa7b72f15c040bca72ff9008f
kent0304 <tanaken0304.0123@gmail.com>
2021-12-18 21:25:45 +0900

    calcurate m2 score in val data and output loss txt file

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-
+import json
+import subprocess
 import torch
 import torch.nn as nn
 import numpy as np
@@ -22,8 +23,8 @@ DataTuple = collections.namedtuple("DataTuple", 'dataset loader')
 # データをバッチサイズでまとめる際に使う関数
 def collate_fn(data):
     # Sort a data list by caption length (descending order).
-    data.sort(key=lambda x: len(x[5]), reverse=True)
-    id, feats, boxes, composition, composition_ids, correction_ids = zip(*data)
+    data.sort(key=lambda x: len(x[-1]), reverse=True)
+    id, filename, feats, boxes, composition, composition_ids, correction, correction_ids = zip(*data)
     # id, feats, boxes, ans_text, crt, file_name, ans_text_copy, ans = zip(*data)
 
     # Merge images (from tuple of 3D tensor to 4D tensor).
@@ -46,7 +47,7 @@ def collate_fn(data):
     #         ans_targets[i, :end] = a[:end]    
     #     else:
     #         ans_targets[i, :30] = a[:30]
-    return id, feats, boxes, composition, composition_ids, targets, lengths
+    return id, filename, feats, boxes, composition, composition_ids, correction, targets, lengths
 
 # drop_lastは最後のミニバッチが余った時にデータを捨てるかどうか
 # pin_memoryはGPUメモリにデータを送るかどうか
@@ -54,6 +55,8 @@ def get_data_tuple(splits: str, bs:int, shuffle=False, drop_last=False) -> DataT
     dset = PDCDataset(splits)
     tset = PDCTorchDataset(dset)
     # (36, 2048)特徴量, (36, 4)位置情報
+    if splits == "raw_val":
+        bs = len(tset)
     data_loader = DataLoader(
         tset, batch_size=bs,
         shuffle=shuffle, num_workers=args.num_workers,
@@ -66,12 +69,20 @@ def get_data_tuple(splits: str, bs:int, shuffle=False, drop_last=False) -> DataT
 class Correct:
     def __init__(self):
         # Datasets
-        self.train_tuple = get_data_tuple(
-            "synthetic_train", bs=args.batch_size, shuffle=True, drop_last=True
-        )
-        self.valid_tuple = get_data_tuple(
-                "synthetic_val", bs=args.batch_size, shuffle=False, drop_last=False
+        if args.data == 'raw':
+            self.train_tuple = get_data_tuple(
+                "raw_train", bs=args.batch_size, shuffle=True, drop_last=True
+            )
+            self.valid_tuple = get_data_tuple(
+                "raw_val", bs=args.batch_size, shuffle=False, drop_last=False
             ) 
+        elif args.data == 'synthetic':
+            self.train_tuple = get_data_tuple(
+                "synthetic_train", bs=args.batch_size, shuffle=True, drop_last=True
+            )
+            self.valid_tuple = get_data_tuple(
+                "synthetic_val", bs=args.batch_size, shuffle=False, drop_last=False
+            )
         self.model = Model()
         
         # # Model
@@ -98,23 +109,40 @@ class Correct:
         # Output Directory
         self.output = args.output
         os.makedirs(self.output, exist_ok=True)
-
-    def evaluate(self, eval_tuple: DataTuple):
+    
+    def evaluate(self, eval_tuple: DataTuple, epoch: int):
         dset, loader = eval_tuple
         self.model.eval()
         valid_loss = 0
         iter_wrapper = (lambda x: tqdm(x, total=len(loader))) if args.tqdm else (lambda x: x)
-        for i, (id, feats, boxes, composition, composition_ids, correction, lengths) in iter_wrapper(enumerate(loader)):
-            feats, boxes,  correction = feats.cuda(), boxes.cuda(),  correction.cuda() # crt (8,30)
-            target = torch.zeros(correction.shape[0], 30).long().cuda()
-            target[:,1:correction.shape[1]] = correction[:,1:] # targetもmax_lengthで覆う必要がある (batchsize, max_length)
+        for i, (id, filename, feats, boxes, composition, composition_ids, correction, targets, lengths) in iter_wrapper(enumerate(loader)):
+            feats, boxes,  targets = feats.cuda(), boxes.cuda(),  targets.cuda() # crt (8,30)
+            target = torch.zeros(targets.shape[0], 30).long().cuda()
+            target[:,1:targets.shape[1]] = targets[:,1:] # targetもmax_lengthで覆う必要がある (batchsize, max_length)
 
-            inputs = crt[:,:-1]
+            inputs = targets[:,:-1]
             with torch.no_grad():
                 # if args.attention or args.copyattention:
                 #     outputs = self.model(feats, boxes, ans_text, inputs, [l-1 for l in lengths], ans_lengths, ans)
                 # else:
                 outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths]) 
+                # m2 score 
+                if args.data == 'raw':
+                    sampled_ids = outputs.max(2)[1] # maxの出力はidx0: 値, idx1: 位置
+                    sentences = [self.model.tokenizer.decode(sampled_id, skip_special_tokens=True) for sampled_id in sampled_ids]
+
+                    with open(os.path.join(self.output, "m2/epoch{}_inference.txt".format(str(epoch))), "w") as f:
+                        for sentence in sentences:
+                            f.write(sentence + "\n")
+                    if not os.path.isfile(os.path.join(self.output, "m2/compositions.txt")):
+                        with open(os.path.join(self.output, "m2/compositions.txt"), "w")as f:
+                            for cp in composition:
+                                f.write(cp + "\n")
+                    if not os.path.isfile(os.path.join(self.output, "m2/references.txt")):
+                        with open(os.path.join(self.output, "m2/references.txt"), "w")as f:
+                            for cr in correction:
+                                f.write(cr + "\n")
+
             loss = self.criterion(outputs.view(-1, 30522), target.reshape(-1))
             valid_loss += loss.item()
         return valid_loss / len(loader)
@@ -125,88 +153,88 @@ class Correct:
         valid_losses = []
         best_valid = 100
         iter_wrapper = (lambda x: tqdm(x, total=len(loader))) if args.tqdm else (lambda x: x)
-        for epoch in range(args.epochs):
-            train_loss = 0
-            self.model.train()
-            for i, (id, feats, boxes, composition, composition_ids, correction, lengths) in iter_wrapper(enumerate(loader)):
-                self.optim.zero_grad()
-                feats, boxes,  correction = feats.cuda(), boxes.cuda(),  correction.cuda() # crt (8,30)
-                target = torch.zeros(correction.shape[0], 30).long().cuda()
+        with open(os.path.join(self.output, "loss.txt"), "w") as losstxt:
+            for epoch in range(args.epochs):
+                train_loss = 0
+                self.model.train()
+                for i, (id, filename, feats, boxes, composition, composition_ids, correction, targets, lengths) in iter_wrapper(enumerate(loader)):
+                    self.optim.zero_grad()
+                    feats, boxes, targets = feats.cuda(), boxes.cuda(),  targets.cuda() # crt (8,30)
+                    target = torch.zeros(targets.shape[0], 30).long().cuda()
 
-                target[:,1:correction.shape[1]] = correction[:,1:] # targetもmax_lengthで覆う必要がある (batchsize, max_length) (8,30)
+                    target[:,1:targets.shape[1]] = targets[:,1:] # targetもmax_lengthで覆う必要がある (batchsize, max_length) (8,30)
 
-                inputs = correction[:,:-1]
+                    inputs = targets[:,:-1]
+                    
+                    # if args.attention or args.copyattention:
+                    #     outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths], ans_lengths, ans)
+                    # else:
+                    #     outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths], ans_lengths)   
+                    outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths]) 
+    
+                    # print(outputs.shape)
+                    
+                    loss = self.criterion(outputs.view(-1, 30522), target.reshape(-1))
+                    loss.backward()
+                    # print(loss.item())
+                    self.optim.step()
+                    train_loss += loss.item()
                 
+                train_losses.append(train_loss / len(loader))
+                valid_loss = self.evaluate(eval_tuple, epoch)
+                valid_losses.append(valid_loss)
+                
+                print(f"Epoch: {epoch} Train Loss: {train_loss / len(loader):.4f} Valid Loss: {valid_loss:.4f}")
+                losstxt.write(f"Epoch: {epoch} Train Loss: {train_loss / len(loader):.4f} Valid Loss: {valid_loss:.4f}\n")
+                self.my_plot(train_losses, valid_losses)
+                # validation データで高い評価値のものを保存
+                if self.valid_tuple is not None:  # Do Validation
+                    if args.data == 'raw':
+                        self.save("epoch{}".format(epoch))
+                        
+                    else:
+                        if valid_loss < best_valid:
+                            best_valid = valid_loss
+                            self.save("BEST")
+
+
+    def predict(self, eval_tuple: DataTuple):
+        self.model.eval()
+        dset, loader = eval_tuple
+        result = {}
+        # tokenizer.decoder = decoders.WordPiece()
+        for i, (id, filename, feats, boxes, composition, composition_ids, correction, targets, lengths) in enumerate(loader): # Avoid seeing ground truth
+            with torch.no_grad():
+                feats, boxes = feats.cuda(), boxes.cuda()
                 # if args.attention or args.copyattention:
-                #     outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths], ans_lengths, ans)
+                #     sampled_ids = self.model.sample(feats, boxes, ans_text, ans_lengths, ans)
                 # else:
-                #     outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths], ans_lengths)   
-                outputs = self.model(feats, boxes, composition, inputs, [l-1 for l in lengths]) 
-                # print(outputs.shape)
-                
-                loss = self.criterion(outputs.view(-1, 30522), target.reshape(-1))
-                loss.backward()
-                # print(loss.item())
-                self.optim.step()
-                train_loss += loss.item()
-               
-            train_losses.append(train_loss / len(loader))
-            valid_loss = self.evaluate(eval_tuple)
-            valid_losses.append(valid_loss)
-            print(f"Epoch: {epoch} Train Loss: {train_loss / len(loader):.4f} Valid Loss: {valid_loss:.4f}")
-            self.my_plot(train_losses, valid_losses)
-            # validation データで高い評価値のものを保存
-            if self.valid_tuple is not None:  # Do Validation
-                if valid_loss < best_valid:
-                    best_valid = valid_loss
-                    self.save("BEST")
-
-
-    # def predict(self, eval_tuple: DataTuple, tokenizer=None, vocab=None):
-    #     self.model.eval()
-    #     dset, loader = eval_tuple
-    #     result = {}
-    #     tokenizer.decoder = decoders.WordPiece()
-    #     for i, (id, feats, boxes, ans_text, crt, lengths, file_name, ans_text_copy, ans_lengths, ans) in enumerate(loader): # Avoid seeing ground truth
-    #         with torch.no_grad():
-    #             feats, boxes = feats.cuda(), boxes.cuda()
-    #             if args.attention or args.copyattention:
-    #                 sampled_ids = self.model.sample(feats, boxes, ans_text, ans_lengths, ans)
-    #             else:
-    #                 sampled_ids = self.model.sample(feats, boxes, ans_text)
-
-    #             sampled_ids = torch.tensor(sampled_ids).cpu().numpy() # (1, max_seq_length) -> (max_seq_length)
-    #         # Convert word_ids to words
-    #         if vocab is None:
-    #             sentence = tokenizer.decode(sampled_ids)
-    #             sentence = sentence.replace(".[SEP]", ". [SEP]") # [SEP]と.が連接しているものを離しておく
-    #             sentence = sentence.replace("[SEP].", "[SEP] .")
-    #             sentence = sentence.replace(" - ", "-")
-    #             sentence_list = sentence.split(" ")
-    #             # sentence_list = tokenizer.tokenize(sentence)
-    #             if "[SEP]" in sentence_list:
-    #                 sep_idx = sentence_list.index("[SEP]")
-    #                 sentence_list = sentence_list[:sep_idx]
-    #                 sentence = " ".join(sentence_list)
-    #                 sentence = sentence.replace(" .", ".")
-    #                 sentence = sentence.replace(" ,", ",")
-    #                 print(sentence)
-    #         else:
-    #             new_sampled_caption = []
-    #             for word_id in sampled_ids:
-    #                 word = vocab.idx2word[word_id]
-    #                 new_sampled_caption.append(word)
-    #                 if word == '<end>':
-    #                     break
-    #             sentence = ' '.join(new_sampled_caption)
-    #         crt = tokenizer.decode(crt.squeeze(0))
-    #         crt = crt.replace("[CLS]", "")
-    #         crt = crt.replace("[SEP]", "")
-    #         crt = crt.replace("[PAD]", "")
-    #         crt = crt.replace(" - ", "-")
-    #         crt = crt.strip()
-    #         result[id[0]] = {"生徒の解答": ans_text_copy[0], "GECToR訂正結果": ans_text[0], "添削アノテーション": crt, "添削予測": sentence, "ファイル名": file_name[0]}
-    #     return result
+                #     sampled_ids = self.model.sample(feats, boxes, ans_text)
+                sampled_ids = self.model.sample(feats, boxes, composition)
+                sampled_ids = torch.tensor(sampled_ids).cpu().numpy() # (1, max_seq_length) -> (max_seq_length)
+            # Convert word_ids to words
+            sentence = self.model.tokenizer.decode(sampled_ids)
+            sentence = sentence.replace(".[SEP]", ". [SEP]") # [SEP]と.が連接しているものを離しておく
+            sentence = sentence.replace("[SEP].", "[SEP] .")
+            sentence = sentence.replace(" - ", "-")
+            sentence_list = sentence.split(" ")
+            # sentence_list = tokenizer.tokenize(sentence)
+            if "[SEP]" in sentence_list:
+                sep_idx = sentence_list.index("[SEP]")
+                sentence_list = sentence_list[:sep_idx]
+                sentence = " ".join(sentence_list)
+                sentence = sentence.replace(" .", ".")
+                sentence = sentence.replace(" ,", ",")
+                print(sentence)
+ 
+            crt = self.model.tokenizer.decode(targets.squeeze(0))
+            crt = crt.replace("[CLS]", "")
+            crt = crt.replace("[SEP]", "")
+            crt = crt.replace("[PAD]", "")
+            crt = crt.replace(" - ", "-")
+            crt = crt.strip()
+            result[id[0]] = {"filename": filename[0], "学習者作文": composition[0], "専門家による添削": correction[0], "モデルによる添削予測": sentence}
+        return result
     
 
 
@@ -242,22 +270,20 @@ def main():
     if args.load is not None:
         correct.load(args.load)
 
-    correct.train(correct.train_tuple, correct.valid_tuple)
 
-    # # Test or Train
-    # if args.test is not None:
-    #     result = correct.predict(
-    #         get_data_tuple(args.test, bs=1, shuffle=False, drop_last=False), 
-    #         tokenizer=tokenizer, 
-    #         vocab=vocab
-    #     )
-    #     with open(os.path.join(args.output, 'test_predict.json'), 'w') as f:
-    #         json.dump(result, f, indent=4, ensure_ascii=False)
+    # Test or Train
+    if args.test is not None:
+        result = correct.predict(
+            get_data_tuple("raw_test", bs=1, shuffle=False, drop_last=False)
+        )
+        # print("========")
+        # print(result)
+        with open(os.path.join(args.output, 'test_predict.json'), 'w') as f:
+            json.dump(result, f, indent=4, ensure_ascii=False)
 
-    # else:
-    #     print('Splits in Train data:', correct.train_tuple.dataset.splits)
-    #     correct.train(correct.train_tuple, correct.valid_tuple)
-
+    else:
+        print('Start training...')
+        correct.train(correct.train_tuple, correct.valid_tuple)
 
     
     
